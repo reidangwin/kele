de9511dc36253f5546ae567c9796c36b96b4deda
Reid Angwin <rangwin@gmail.com>
2017-11-06 14:19:58 -0500

    5 Roadmaps and Checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,11 +1,18 @@
 require 'httparty'
 require 'json'
+require './lib/roadmap'
 
 class Kele
   include HTTParty
+  include Roadmap
 
   @bloc_api_url = 'https://www.bloc.io/api/v1'
   base_uri @bloc_api_url
+
+  def api_url(end_point)
+    "https://www.bloc.io/api/v1/#{end_point}"
+  end
+
 
   def initialize(email, password)
     @email = email
@@ -22,12 +29,12 @@ class Kele
   end
 
   def get_me
-    response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
     @user = JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
     @mentor_availability = JSON.parse(response.body)
   end
 end
