cbc1f955745cae12fc5085d79b2cb212ff8c6dab
Reid Angwin <rangwin@gmail.com>
2017-11-07 05:19:32 -0500

    6 Messaging

--- lib/kele.rb
+++ lib/kele.rb
@@ -29,12 +29,37 @@ class Kele
   end
 
   def get_me
-    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url("users/me"), headers: {"authorization" => @auth_token})
     @user = JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token})
     @mentor_availability = JSON.parse(response.body)
+  end
+
+  def get_messages(page=nil)
+    if page
+      response = self.class.get(api_url("message_threads"), body: {page: page}, headers: {"authorization" => @auth_token})
+      @messages = JSON.parse(response.body)
+    else
+      response = self.class.get(api_url("message_threads"), body: {page: 1}, headers: {"authorization" => @auth_token})
+      @messages = (1..(response['count']/10+1)).map do |p|
+        self.class.get(api_url("message_threads"), body: {page: p}, headers: {"authorization" => @auth_token})
+      end
+      @messages
+    end
+  end
+
+  def create_message(sender, recipient_id, token, subject, message)
+    options = {
+        'sender': sender,
+        'recipient_id': recipient_id,
+        'token': token,
+        'subject': subject,
+        'stripped-text': message
+    }
+
+    self.class.post(api_url("messages"), body: options, headers: {"authorization" => @auth_token})
   end
 end
